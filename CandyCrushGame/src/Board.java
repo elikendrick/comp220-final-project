c80abb5ba5378952231cf16a973157ab3d76e198
Sydnee S. Charles <sydnee12sarah@yahoo.fr>
2019-11-12 21:40:59 -0500

    RefreshBoard() fillSpaces()

--- CandyCrushGame/src/Board.java
+++ CandyCrushGame/src/Board.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Board {
 	
 	private final int width, height; //number of blocks in each dimension
-	
+	private Random rand;
 	ArrayList<Integer> tiles; //IDs of block on board, empty space designated by Integer.minValue()
 	
 	/**
@@ -22,21 +23,50 @@ public class Board {
 	public Board(int width, int height) {
 		this.width = width;
 		this.height = height;
-		
+		tiles = new ArrayList<>();
+		rand = new Random();
 	}
 	
 	/**
 	 * refills total board with random tiles
 	 */
 	public void refreshBoard() {
-		
+		for(int h =0; h < height; h++) {
+			for(int w = 0; w < width; w++) {
+				int c = rand.nextInt(6); //6 different color block
+				tiles.add(c);
+			}
+			//new row
+		}
 	}
 	
 	/**
 	 * fill all empty spaces after a move
 	 */
 	public void fillSpaces() {
-		
+		for(int i = tiles.size(); i > 0; i--) {// down to up
+			
+			if(tiles.get(i) == Integer.MIN_VALUE) { //Check if empty
+				boolean emptyC = false;	//empty column
+				
+				for(int j = 1; (i - j*width)> height; j++) { //(i-j*width) the # of block on top
+					if(tiles.get(i - j*width) == Integer.MIN_VALUE) {
+						emptyC = true; 
+					}
+					else {	//Switch top block 
+						int switchValue = tiles.get(i- j*width);
+						tiles.set(i-j*width, Integer.MIN_VALUE);
+						tiles.set(i, switchValue);
+						emptyC = false;
+						break;
+					}
+				}
+				if(emptyC) {
+					tiles.set(i, rand.nextInt(6)); // when column empty block replace by a new
+				}
+			}
+		} 
+			
 	}
 	
 	/**
